ea2cde0d555786f8c7e49c7f7e3dcf9728def977
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-09-24 20:25:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(array)
   array.each{|i| puts i}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)# code an argument here
+ arr.collect{|x| x.capi # Your code here
 end
 
 def long_planeteer_calls# code an argument here
