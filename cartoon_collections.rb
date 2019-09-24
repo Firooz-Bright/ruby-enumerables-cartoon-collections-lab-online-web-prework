dc77b2211237e996c171b22156ff17937cc50843
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-09-24 20:40:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(arry)# code an argument here
  arry.collect{|i| return true if i>4 } # Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)# code an argument here
+  arr.collect {|i| if i=="cheddar"|| i=="gouda"|| i=="camembert" return i }  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
