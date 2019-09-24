d1f35e7ff9e322642031edb5ddb67c25624ff868
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-09-24 20:27:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(arr)# code an argument here
- arr.collect{|x| x.capi # Your code here
+ arr.collect{|x|  puts x.capitalize +"!"} # Your code here
 end
 
 def long_planeteer_calls# code an argument here
